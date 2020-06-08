de6ca1ce073c51d0a878407366ed42b55eec0619
caiolouro <caiolouro@gmail.com>
2020-06-08 19:28:44 -0300

    First working get cities route

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")] // Global route
+    public class CitiesController : ControllerBase
+    {
+        public CitiesController()
+        {
+        }
+
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id = 1, Name = "Rio de Janeiro" },
+                new { id = 2, Name = "São Paulo" }
+            });
+        }
+    }
+}
