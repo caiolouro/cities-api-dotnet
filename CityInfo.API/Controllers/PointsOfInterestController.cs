037a4caf7544d47b8f1d2a4a091ed0d792b360d9
caiolouro <caiolouro@gmail.com>
2020-06-10 14:22:02 -0300

    Added POST request to city point of interest

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -25,7 +26,7 @@ namespace CityInfo.API.Controllers
             return Ok(existingCity.PointsOfInterest);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var existingCity = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
@@ -43,6 +44,44 @@ namespace CityInfo.API.Controllers
             }
 
             return Ok(existingPointOfInterest);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            // No need to check if pointOfInterest is valid because the ApiController property for the controller enforces that.
+            // So, if the body is not valid and it can't be serialized to a PointOfInterestForCreationDto, it will return a bad request response automatically.
+
+            // Custom validation that the DTO can't do
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "The Name and Description can't be the same.");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(auxCity => auxCity.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            // Bad code, just for testing
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(auxCity => auxCity.PointsOfInterest).Max(auxPointOfInterest => auxPointOfInterest.Id);
+
+            var pointOfInterestDto = new PointsOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(pointOfInterestDto);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId, id = pointOfInterestDto.Id }, pointOfInterestDto);
         }
     }
 }
---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        public PointOfInterestForCreationDto()
+        {
+        }
+
+        // You can configure a custom error message
+        [Required(ErrorMessage = "Missing the name.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
